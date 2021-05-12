c927a73d6f12bad850c459bbb5a2eccd280d0406
Minqiu Yu <minqiu.yu@yahoo.com>
2021-05-12 05:28:03 -0400

    Uploading React project prototype

---
+++ login-page/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Login</h1>
+        <p className="text-muted">Creating an example login page</p>
+      </div>
+    </div>
+  );
+}
